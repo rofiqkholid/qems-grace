60e055e759daa3f58dd42b050fe2cb70587881b6
Rofiq Kholid <rofiqgege99@gmail.com>
2026-07-14 10:27:57 +0700

    feat: import Outfit font weights in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,10 @@
 import './bootstrap';
-import '@fontsource/outfit';
+import '@fontsource/outfit/100.css';
+import '@fontsource/outfit/200.css';
+import '@fontsource/outfit/300.css';
+import '@fontsource/outfit/400.css';
+import '@fontsource/outfit/500.css';
+import '@fontsource/outfit/600.css';
+import '@fontsource/outfit/700.css';
+import '@fontsource/outfit/800.css';
+import '@fontsource/outfit/900.css';
